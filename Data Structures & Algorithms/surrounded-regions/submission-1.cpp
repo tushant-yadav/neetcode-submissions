3422927116c347854366d88d524fe16f6972a7b8
Tushant Yadav <56768158+tushant-yadav@users.noreply.github.com>
2026-04-04 12:52:47 +0530

    Add: surrounded-regions - submission-1

---
+++ Data Structures & Algorithms/surrounded-regions/submission-1.cpp
@@ -0,0 +1,39 @@
+class Solution {
+    int n , m;
+public:
+
+    void fill(vector<vector<char>>& board, int i, int j){
+        if(i<0 || i>=n || j<0 || j>=m || board[i][j]!='O') return;
+        board[i][j] = '-';
+        fill(board,i-1,j);
+        fill(board,i+1,j);
+        fill(board,i,j+1);
+        fill(board,i,j-1);
+    }
+    void solve(vector<vector<char>>& board) {
+        n = board.size();
+        m = board[0].size();
+        if(n<3||m<3) return;
+        // for(int i=0;i<n;i++){
+        //     for(int j=0;j<m;j++){
+        //         if(board[i][j]=='O') board[i][j] = '-';
+        //     }
+        // }
+
+        for(int i=0;i<n;i++){
+            fill(board,i,0);
+            fill(board,i,m-1);
+        }
+        for(int j=0;j<m;j++){
+            fill(board,0,j);
+            fill(board,n-1,j);
+        }
+
+        for(int i=0;i<n;i++){
+            for(int j=0;j<m;j++){
+                if(board[i][j]=='-') board[i][j] = 'O';
+                else if(board[i][j]=='O') board[i][j] = 'X';
+            }
+        }
+    }
+};
