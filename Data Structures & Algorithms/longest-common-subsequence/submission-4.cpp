93e3e9104282134c038957842ff2f8cb839f45c8
Tushant Yadav <56768158+tushant-yadav@users.noreply.github.com>
2026-04-05 23:55:21 +0530

    Add: longest-common-subsequence - submission-4

---
+++ Data Structures & Algorithms/longest-common-subsequence/submission-4.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    int longestCommonSubsequence(string text1, string text2) {
+        int n = text1.size(), m = text2.size();
+        if(n==0||m==0) return 0;
+        // vector<vector<int>> dp(n+1,vector<int>(m+1,0));
+        vector<int> dp(m+1,0);
+        vector<int> temp(m+1);
+        for(int i=n-1;i>=0;i--){
+            temp.assign(dp.begin(),dp.end());
+            for(int j=m-1;j>=0;j--){
+                if(text1[i]==text2[j]){
+                    // dp[i][j] = 1 + dp[i+1][j+1];
+                    dp[j] = 1 +  temp[j+1];
+                }
+                else{
+                    dp[j] = max(dp[j+1], temp[j]);
+                }
+            }
+        }
+        return dp[0];
+        
+    }
+};
